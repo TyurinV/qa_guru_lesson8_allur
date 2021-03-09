af757a4a8ea2f219504448396b3bcda7e21e86e7
TyurinV <denfal486@gmailc.om>
2021-03-09 22:25:08 +0300

    Added attach Helpers

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,10 +2,13 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
 
 public class TestBase {
     @BeforeAll
@@ -17,12 +20,22 @@ public class TestBase {
         Configuration.startMaximized = true;
 
 
-            // config for Java + Selenide
-          //  DesiredCapabilities capabilities = new DesiredCapabilities();
-           // capabilities.setCapability("enableVNC", true);
-           // capabilities.setCapability("enableVideo", true);
-           // Configuration.browserCapabilities = capabilities;
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+        // config for Java + Selenide
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
 
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
     }
 }
