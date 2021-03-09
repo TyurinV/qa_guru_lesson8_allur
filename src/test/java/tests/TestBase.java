c0aac398468705894e98ca2bc0eaea5128b82a55
TyurinV <denfal486@gmailc.om>
2021-03-09 22:11:54 +0300

    Added confige for remote start

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,28 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.startMaximized = true;
+
+
+            // config for Java + Selenide
+          //  DesiredCapabilities capabilities = new DesiredCapabilities();
+           // capabilities.setCapability("enableVNC", true);
+           // capabilities.setCapability("enableVideo", true);
+           // Configuration.browserCapabilities = capabilities;
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/"
+
     }
 }
