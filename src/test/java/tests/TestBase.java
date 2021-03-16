996abcd45e0fdb4d9d5b1ea444d01609c8624c81
TyurinV <denfal486@gmailc.om>
2021-03-16 10:17:19 +0300

    Added console log

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -33,6 +33,7 @@ public class TestBase {
     public void afterEach() {
         attachScreenshot("Last screenshot");
         attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
         attachVideo();
         closeWebDriver();
     }
